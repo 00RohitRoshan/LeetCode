c19bfa67a9053f50f01ab6072acb94d877bf054b
Rohit Roshan <105505328+00RohitRoshan@users.noreply.github.com>
2023-08-18 09:13:42 +0530

    Time: 60 ms (93.33%), Space: 30.7 MB (81.20%) - LeetHub

---
+++ 1615-maximal-network-rank/1615-maximal-network-rank.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maximalNetworkRank(int n, vector<vector<int>>& roads) {
+        int count[101] = {0};
+        int direct[101][101] = {0};
+        for(auto road: roads){
+            count[road[0]]++;
+            count[road[1]]++;
+            direct[road[0]][road[1]] = 1;
+            direct[road[1]][road[0]] = 1;
+        }
+        
+        int rank =0;
+        for(int i =0; i<n; i++){
+            for(int j = i+1; j<n; j++){
+               if(i!=j){ rank = max(rank, count[i]+count[j]-direct[i][j]);}
+            }
+        }
+        return rank;
+    }
+};
