b3f4eae72052e01e70ace3eb1ca07f84bef3e327
Rohit Roshan <105505328+00RohitRoshan@users.noreply.github.com>
2023-09-03 11:34:17 +0530

    Time: 211 ms (43.95%), Space: 107.5 MB (26.74%) - LeetHub

--- 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.cpp
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.cpp
@@ -1,20 +1,26 @@
 class Solution {
 public:
     int minExtraChar(string s, vector<string>& dictionary) {
-        int max_val = s.length() + 1;
-        vector<int> dp(s.length() + 1, max_val);
-        dp[0] = 0;
-
         unordered_set<string> dictionary_set(dictionary.begin(), dictionary.end());
-
-        for (int i = 1; i <= s.length(); ++i) {
-            dp[i] = dp[i - 1] + 1;
-            for (int l = 1; l <= i; ++l) {
-                if (dictionary_set.find(s.substr(i - l, l)) != dictionary_set.end()) {
-                    dp[i] = min(dp[i], dp[i - l]);
-                }
+        vector<int> dp(s.length() + 1, -1);
+        return minExtraCharHelper(s, 0, dictionary_set, dp);
+    }
+    
+    int minExtraCharHelper(string s, int start, unordered_set<string>& dictionary_set, vector<int>& dp) {
+        if (start == s.length()) {
+            return 0;
+        }
+        if (dp[start] != -1) {
+            return dp[start];
+        }
+        int max_val = s.length() + 1;
+        dp[start] = max_val;
+        for (int i = start + 1; i <= s.length(); ++i) {
+            if (dictionary_set.find(s.substr(start, i - start)) != dictionary_set.end()) {
+                dp[start] = min(dp[start], minExtraCharHelper(s, i, dictionary_set, dp));
             }
         }
-        return dp.back();
+        dp[start] = min(dp[start], minExtraCharHelper(s, start + 1, dictionary_set, dp) + 1);
+        return dp[start];
     }
 };
