9052a3b5314bd0be3f416487431414b563125afd
Rohit Roshan <105505328+00RohitRoshan@users.noreply.github.com>
2023-09-07 12:05:27 +0530

    Time: 14 ms (35.08%), Space: 15.8 MB (95.25%) - LeetHub

---
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -0,0 +1,61 @@
+/*
+
+    Time Complexity : O(N), Boyer-Moore performs constant work exactly N times, so the algorithm runs in
+    linear time. Where N is the size of the Array(nums).
+
+    Space Complexity : O(1), Constant Space. Because we are creating Array(output) as per the demand of
+    program so it doesnot count as auxilary space also Array(output) will maximum store 2 element not more than
+    that which is a constant. 
+
+    Solved using Boyer-Moore Voting Algorithm.
+
+*/
+
+
+/***************************************** Approach 4 *****************************************/
+
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        int num1 = INT_MIN, num2 = INT_MIN;
+        int count1 = 0, count2 = 0;
+        for(auto element : nums){
+            if(num1 == element){
+                count1++;
+            }
+            else if(num2 == element){
+                count2++;
+            }
+            else if(count1 == 0){
+                num1 = element;
+                count1 = 1;
+            }
+            else if(count2 == 0){
+                num2 = element;
+                count2 = 1;
+            }
+            else{
+                count1--;
+                count2--;
+            }
+        }
+        vector<int> output;
+        int countMajority = nums.size()/3;
+        count1 = 0, count2 = 0;
+        for(auto element : nums){
+            if(num1 == element){
+                count1++;
+            }
+            if(num2 == element){
+                count2++;
+            }
+        }
+        if(count1 > countMajority){
+            output.push_back(num1);
+        }
+        if(count2 > countMajority){
+            output.push_back(num2);
+        }
+        return output;
+    }
+};
