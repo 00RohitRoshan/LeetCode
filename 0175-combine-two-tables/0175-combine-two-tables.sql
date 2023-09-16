ace24e76d6c34c554ab48166fc5858cc7a0d2381
Rohit Roshan <105505328+00RohitRoshan@users.noreply.github.com>
2023-09-16 10:37:58 +0530

    Time: 989 ms (50.16%), Space: 0B (100.00%) - LeetHub

--- 0175-combine-two-tables/0175-combine-two-tables.sql
+++ 0175-combine-two-tables/0175-combine-two-tables.sql
@@ -1,2 +1,4 @@
-/* Write your PL/SQL query statement below */
-Select firstName,lastName,city,state From Person Left Outer Join Address On (Person.personId=Address.personId)
+/* Write your T-SQL query statement below */
+SELECT P.firstName, P.lastName, A.city, A.state
+FROM Person P LEFT JOIN Address A
+on P.personId = A.personId
