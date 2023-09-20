fa07cf22385dd57eaa950704df7fc1902aa82734
Rohit Roshan <105505328+00RohitRoshan@users.noreply.github.com>
2023-09-20 20:22:25 +0530

    Update StringGeneration.cpp

--- 0000-OT/StringGeneration/StringGeneration.cpp
+++ 0000-OT/StringGeneration/StringGeneration.cpp
@@ -8,6 +8,7 @@ int main() {
 
     int L1 = S1.length();
     int L2 = S2.length();
+    char smallest_char = *min_element(S1.begin(), S1.end());
     string result;
 
     for (int i = 0; i < L2; i++) {
@@ -16,11 +17,13 @@ int main() {
             result += S1;
         } else {
             // If S2[i] is 'F', append the lexicographically smallest character of S1.
-            char smallest_char = *min_element(S1.begin(), S1.end());
             result += smallest_char;
         }
     }
-
+    for (int a = result.size();a< L1 + L2 - 1; a++){
+            result+=smallest_char;
+    }
+    
     // Check if the length of the result is (L1 + L2 - 1).
     if (result.length() != (L1 + L2 - 1)) {
         cout << "-1" << endl;
