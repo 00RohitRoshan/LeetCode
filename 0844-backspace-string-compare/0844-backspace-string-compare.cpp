87117d3e7e8a3f3e9c66125e5d97018bd2bfd170
Rohit Roshan <105505328+00RohitRoshan@users.noreply.github.com>
2023-10-19 08:20:13 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (39.46%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.cpp
+++ 0844-backspace-string-compare/0844-backspace-string-compare.cpp
@@ -1,17 +1,22 @@
 class Solution {
 public:
-    string build(string str) {
-        string result;
-        for (char c : str) {
-            if (c != '#')
-                result.push_back(c);
-            else if (!result.empty())
-                result.pop_back();
-        }
-        return result;
-    }
-
     bool backspaceCompare(string s, string t) {
-        return build(s) == build(t);
+        int i = s.length() - 1, j = t.length() - 1;
+        int backspace_s = 0, backspace_t = 0;
+
+        while (true) {
+            while (i >= 0 && (backspace_s || s[i] == '#')) {
+                backspace_s += s[i] == '#' ? 1 : -1;
+                i--;
+            }
+            while (j >= 0 && (backspace_t || t[j] == '#')) {
+                backspace_t += t[j] == '#' ? 1 : -1;
+                j--;
+            }
+            if (!(i >= 0 && j >= 0 && s[i] == t[j])) {
+                return i == -1 && j == -1;
+            }
+            i--; j--;
+        }
     }
 };
