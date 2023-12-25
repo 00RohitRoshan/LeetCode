cdc1aa722f09baab70e59ce4c20f267cd242f6e6
Rohit Roshan <105505328+00RohitRoshan@users.noreply.github.com>
2023-12-25 11:42:21 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (87.76%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -17,17 +17,35 @@
 // };
 
 // Bottom Up
+// class Solution {
+// public:
+//      int numDecodings(string s) {
+//         int n = s.size();
+//         vector<int> dp(n+1);
+//         dp[n] = 1;
+//         for(int i=n-1;i>=0;i--) {
+//             if(s[i]=='0') dp[i]=0;
+//             else {
+//                 dp[i] = dp[i+1];
+//                 if(i<n-1 && (s[i]=='1'||s[i]=='2'&&s[i+1]<'7')) dp[i]+=dp[i+2];
+//             }
+//         }
+//         return s.empty()? 0 : dp[0];   
+//      }
+// };
+
+// Space optimization
 class Solution {
 public:
-    int  numDecodings(string s) {
-        if (s.empty()) return 0;
-        int n = s.size();
-        vector<int> dp(n+1,0);
-        dp[n] = 1;
-        for(int i = n-1; i >=0;i--){
-            if (s[i]!='0') dp[i] = dp[i+1];
-            if (i < n-1 && (s[i] == '1' || (s[i] == '2' && s[i+1] < '7'))) dp[i]=dp[i]+dp[i+2];
+    int numDecodings(string s) {
+        // p = previous
+        int p = 1, pp, n = s.size();
+        for(int i=n-1;i>=0;i--) {
+            int cur = s[i]=='0' ? 0 : p;
+            if(i<n-1 && (s[i]=='1'||s[i]=='2'&&s[i+1]<'7')) cur+=pp;
+            pp = p;
+            p = cur;
         }
-        return dp[0];
+        return s.empty()? 0 : p;   
     }
 };
