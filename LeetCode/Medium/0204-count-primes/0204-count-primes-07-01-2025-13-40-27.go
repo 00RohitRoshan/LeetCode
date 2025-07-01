ea076f5afd1d3480466381be139a1cc248fb54c9
Rohit Roshan <105505328+00RohitRoshan@users.noreply.github.com>
2025-07-01 13:40:28 +0530

    Time: 93 ms (35.26%), Space: 14.4 MB (44.44%) - LeetHub

---
+++ LeetCode/Medium/0204-count-primes/0204-count-primes-07-01-2025-13-40-27.go
@@ -0,0 +1,22 @@
+func countPrimes(n int) int {
+    nums := make([]bool,n)
+    for i := 2 ; i<n ; i++{
+        nums[i] = true
+    }
+    for i := 2 ; i<n ; i++{
+        if nums[i] {
+            for j := i*i; j<n; j+=i{
+                nums[j] = false
+                // fmt.Println(j)
+            }
+        }
+    }
+    ans := 0
+    // fmt.Println(nums)
+    for _,B :=  range nums{
+        if B {
+            ans++
+        }
+    }
+    return ans
+}
