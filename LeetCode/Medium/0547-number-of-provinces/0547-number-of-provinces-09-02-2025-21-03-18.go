35b8ced0d539275c14e3b7d2c93597f52f2a2dc2
Rohit Roshan <105505328+00RohitRoshan@users.noreply.github.com>
2025-09-02 21:03:18 +0530

    Time: 3 ms (20%), Space: 8.4 MB (73%) - LeetHub

---
+++ LeetCode/Medium/0547-number-of-provinces/0547-number-of-provinces-09-02-2025-21-03-18.go
@@ -0,0 +1,26 @@
+func findCircleNum(isConnected [][]int) int {
+    n := len(isConnected)
+    visited := map[int]bool{}
+    count := 0
+    q := []int{}
+    for i := range n {
+        if !visited[i] {
+            count++
+            visited[i] = true
+            q = append(q, i)
+
+            for len(q) > 0 {
+                cur := q[0]
+                q = q[1:] // dequeue
+
+                for j := range n {
+                    if isConnected[cur][j] == 1 && !visited[j] {
+                        visited[j] = true
+                        q = append(q, j)
+                    }
+                }
+            }
+        }
+    }
+    return count
+}
