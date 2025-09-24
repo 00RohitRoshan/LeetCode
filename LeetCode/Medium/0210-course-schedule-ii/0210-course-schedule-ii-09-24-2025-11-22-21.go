7ae360fec02912d519e27aa62fac9458bdf5c687
Rohit Roshan <105505328+00RohitRoshan@users.noreply.github.com>
2025-09-24 11:22:22 +0530

    0210-course-schedule-ii - time : 0 ms (100%)  space : 7 MB (85.5%) - 09-24-2025 - Medium - .go

---
+++ LeetCode/Medium/0210-course-schedule-ii/0210-course-schedule-ii-09-24-2025-11-22-21.go
@@ -0,0 +1,38 @@
+func findOrder(numCourses int, prerequisites [][]int) []int {
+    adj := make([][]int,numCourses)
+    inDegree := make([]int,numCourses)
+    for _,course := range prerequisites {
+        cur,pre := course[0],course[1]
+        adj[pre] = append(adj[pre],cur)
+        inDegree[cur]++
+    }
+    leaves := []int{}
+    for cur,numPre := range inDegree {
+        if numPre == 0 {
+            leaves = append(leaves,cur)
+        }
+    }
+    n := numCourses
+    ans := []int{}
+    for n > 0{
+        if len(leaves) == 0 {
+            break
+        }
+        n -= len(leaves)
+        nextLeaves := []int{}
+        for _,leaf := range leaves {
+            ans = append(ans,leaf)
+            for _,next := range adj[leaf]{
+                inDegree[next]--
+                if inDegree[next] == 0 {
+                    nextLeaves = append(nextLeaves,next)
+                }
+            }
+        }
+        leaves = nextLeaves
+    }
+    if len(ans) != numCourses {
+        return []int{}
+    }
+    return ans
+}
