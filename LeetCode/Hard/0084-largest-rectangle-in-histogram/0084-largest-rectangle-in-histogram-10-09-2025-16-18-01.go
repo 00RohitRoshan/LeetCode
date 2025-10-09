f706cf140df7c733f8b525300712b42bde25eac1
Rohit Roshan <105505328+00RohitRoshan@users.noreply.github.com>
2025-10-09 16:18:03 +0530

    0084-largest-rectangle-in-histogram - time : 11 ms (71.91%)  space : 11.3 MB (51.33%) - 10-09-2025 - Hard - .go

---
+++ LeetCode/Hard/0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram-10-09-2025-16-18-01.go
@@ -0,0 +1,49 @@
+// O(3n)
+func largestRectangleArea(heights []int) int {
+    numberOfHeights := len(heights)
+    stack := []int{}
+    left := make([]int,numberOfHeights)
+    right := make([]int,numberOfHeights)
+    for i:= range numberOfHeights{ 
+        for len(stack)>0 && heights[stack[len(stack)-1]] >= heights[i] {
+            stack = stack[:len(stack)-1]
+        }
+        if len(stack) == 0 {
+            left[i] = 0
+        }else{
+            left[i] = stack[len(stack)-1]+1
+        }
+        stack = append(stack,i)
+    }
+    stack = []int{}
+    for i:=numberOfHeights-1; i>=0; i--{ 
+        for len(stack)>0 && heights[stack[len(stack)-1]] >= heights[i] {
+            stack = stack[:len(stack)-1]
+        }
+        if len(stack) == 0 {
+            right[i] = numberOfHeights-1
+        }else{
+            right[i] = stack[len(stack)-1]-1
+        }
+        stack = append(stack,i)
+    }
+    maxheight := math.MinInt
+    for i := range numberOfHeights{
+        maxheight = max(maxheight,(right[i]-left[i]+1)*heights[i])
+    }
+    return maxheight
+}
+
+// // TLE
+// func largestRectangleArea(heights []int) int {
+//     n := len(heights)
+//     maxArea := math.MinInt
+//     for i := 0; i<n; i++{
+//         minHeight := math.MaxInt
+//         for j := i; j<n; j++{
+//             minHeight = min(minHeight,heights[j])
+//             maxArea = max(maxArea,minHeight*(j-i+1))
+//         }
+//     }
+//     return maxArea
+// }
