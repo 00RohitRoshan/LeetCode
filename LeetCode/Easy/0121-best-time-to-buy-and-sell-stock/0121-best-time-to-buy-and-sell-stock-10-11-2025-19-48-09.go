1b2ff27ec62531132c33c7e3b22401291d8b1887
Rohit Roshan <105505328+00RohitRoshan@users.noreply.github.com>
2025-10-11 19:48:09 +0530

    0121-best-time-to-buy-and-sell-stock - time : 4 ms (7.53%)  space : 9.5 MB (91.1%) - 10-11-2025 - Easy - .go

---
+++ LeetCode/Easy/0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock-10-11-2025-19-48-09.go
@@ -0,0 +1,28 @@
+func maxProfit(prices []int) int {
+    n := len(prices)
+    mem := make([]int,n)
+    mem[0] = prices[0]
+    maxProfit := 0 
+    for i := 1; i<n; i++ {
+        maxProfit = max(maxProfit,prices[i]-mem[i-1])
+        mem[i] = min(mem[i-1],prices[i])
+        // fmt.Printf("mem %+v maxprofit %d i %d\n",mem,maxProfit,i)
+    }
+    return maxProfit
+}
+
+
+
+// // n^2 TLE
+// func maxProfit(prices []int) int {
+//     maxProfit := 0
+//     n := len(prices)
+//     for i := range n  {
+//         for j := i; j<n; j++{
+//             if prices[j] - prices[i] > maxProfit {
+//                 maxProfit = prices[j] - prices[i]
+//             }
+//         }
+//     }
+//     return maxProfit
+// }
