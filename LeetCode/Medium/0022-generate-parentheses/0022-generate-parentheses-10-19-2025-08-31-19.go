0e73294f767fa99f102e8335eedba80a0ddd2500
Rohit Roshan <105505328+00RohitRoshan@users.noreply.github.com>
2025-10-19 08:31:18 +0530

    0022-generate-parentheses - time : 0 ms (100%)  space : 4.6 MB (97.05%) - 10-19-2025 - Medium - .go

---
+++ LeetCode/Medium/0022-generate-parentheses/0022-generate-parentheses-10-19-2025-08-31-19.go
@@ -0,0 +1,20 @@
+func generateParenthesis(n int) []string {
+    s := make([]byte,2*n)
+    res := []string{}
+    f(0,0,&s,n,&res)
+    return res
+}
+func f(open int,close int,s *[]byte,n int,res *[]string){
+    if open == close && open + close == 2*n {
+        *res = append(*res,string(*s))
+        return
+    }
+    if open < n {
+        (*s)[open+close] = '('
+        f(open+1,close,s,n,res)
+    }
+    if open > close {
+        (*s)[open+close] = ')'
+        f(open,close+1,s,n,res)
+    }
+}
