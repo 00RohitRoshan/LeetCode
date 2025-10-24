c861369453406b7066c7c3564b31cdc55bf98743
Rohit Roshan <105505328+00RohitRoshan@users.noreply.github.com>
2025-10-24 10:29:27 +0530

    0283-move-zeroes - time : 0 ms (100%)  space : 8.7 MB (65.81%) - 10-24-2025 - Easy - .go

---
+++ LeetCode/Easy/0283-move-zeroes/0283-move-zeroes-10-24-2025-10-29-26.go
@@ -0,0 +1,11 @@
+func moveZeroes(nums []int)  {
+    i,j := 0,0
+    n := len(nums)
+    for j < n{
+        if nums[j] != 0 {
+            nums[i],nums[j] = nums[j],nums[i]
+            i++
+        }
+        j++
+    }
+}
