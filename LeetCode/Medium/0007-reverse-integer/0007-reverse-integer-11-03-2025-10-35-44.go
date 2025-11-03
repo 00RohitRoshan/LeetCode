54b617e2a39032ad9e12b90173e818a09b332ece
Rohit Roshan <105505328+00RohitRoshan@users.noreply.github.com>
2025-11-03 10:35:45 +0530

    0007-reverse-integer - time : 3 ms (25.42%)  space : 4.1 MB (27.62%) - 11-03-2025 - Medium - .go

---
+++ LeetCode/Medium/0007-reverse-integer/0007-reverse-integer-11-03-2025-10-35-44.go
@@ -0,0 +1,17 @@
+func reverse(x int) int {
+    // sign := 1 
+    // if x < 0 {
+    //     sign = -1
+    //     x *= -1
+    // }
+    res := 0
+    for x != 0 {
+        m := x%10
+        res = (res*10)+m
+        if res >= math.MaxInt32 || res <= math.MinInt32 {
+            return 0
+        }
+        x = (x-m)/10
+    }
+    return res
+}
