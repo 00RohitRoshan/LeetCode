6157b864abd51473d2bf2bb0cdf519fd10b2ae46
Rohit Roshan <105505328+00RohitRoshan@users.noreply.github.com>
2025-11-04 21:05:17 +0530

    0704-binary-search - time : 0 ms (100%)  space : 8.8 MB (55.24%) - 11-04-2025 - Easy - .go

---
+++ LeetCode/Easy/0704-binary-search/0704-binary-search-11-04-2025-21-05-17.go
@@ -0,0 +1,15 @@
+func search(nums []int, target int) int {
+    i,j := 0,len(nums)-1
+    for i<=j {
+        m:= i+((j-i)/2)
+        // fmt.Println(m,nums[m])
+        if nums[m] > target {
+            j = m-1
+        }else if nums[m] < target{
+            i = m+1
+        }else{
+            return m
+        }
+    }
+    return -1
+}
