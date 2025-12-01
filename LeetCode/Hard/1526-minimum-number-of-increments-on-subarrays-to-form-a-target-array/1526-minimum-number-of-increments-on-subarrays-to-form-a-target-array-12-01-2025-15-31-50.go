1e0f85b24986d95c2bfbf46a03651baa97ea509d
Rohit Roshan <105505328+00RohitRoshan@users.noreply.github.com>
2025-12-01 15:31:51 +0530

    1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array - time : 606 ms (5.02%)  space : 10.5 MB (35.91%) - 12-01-2025 - Hard - .go

---
+++ LeetCode/Hard/1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array/1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array-12-01-2025-15-31-50.go
@@ -0,0 +1,23 @@
+func minNumberOperations(target []int) int {
+    n := len(target)
+    sum := 0
+    start := 0 
+    i := 0
+    for i < n {
+        for i<n && target[i] >= start {
+            start = target[i]
+            i++
+        }
+        fmt.Println(start)
+        sum += start
+        for i<n && target[i] <= start {
+            start = target[i]
+            i++
+        }
+        if i != n {
+            fmt.Println(n,i,-start)
+            sum -= start
+        }
+    }
+    return sum
+}
